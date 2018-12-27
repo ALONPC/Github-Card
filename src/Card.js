e90bca1c5d743cab103c9d631297285a701edb69
AL_ON_PC <alon.berrios@gmail.com>
2018-12-27 10:29:31 -0300

    Initial commit

--- src/Card.js
+++ src/Card.js
@@ -4,12 +4,13 @@ import React, { Component } from 'react';
 class Card extends Component{
     render(){
         let hasCompany = this.props.company != null;
+        let hasName = this.props.name != null;
         return(
             <div className='info'>
                 <img className='picture' src={this.props.avatar_url}></img>
                 <div className='info'>
                     <div className='nickname'>{this.props.login}</div>
-                    <div className='name'>{this.props.name}</div>
+                    <div className='name'>{ hasName ? this.props.name : 'No name' }</div>
                     <div className='company'>{ hasCompany ? this.props.company : 'No company' }</div>
                 </div>
             </div>
